e659be9a6ccad725a06fda025ff6068799c18e76
Parth Bisht <bishtparth795@gmail.com>
2022-06-23 00:55:28 +0530

    completed

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:8080";
+export const host = "https://chatappbyparth.herokuapp.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
